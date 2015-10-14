1a950f0a3ac3ff4101f1de15ea9129008cad6eb7
Shay Bagants <shayb@jfrog.com>
2015-10-14 18:41:31 +0300

    adding bottle do
    adding bottle do

--- formalfridayclub.rb
+++ formalfridayclub.rb
@@ -5,6 +5,10 @@ class Formalfridayclub < Formula
   version "1.0.0"
   sha256 "3e43f927ce1d6e739130442769d77a0084d42d9e92f454d4375159ab7c41f4a1"
 
+  bottle do{
+  	sha1 "30cc3ed88b32c6113ba5ea4cfacd7ad7b3b96c2e" => :yosemite
+  }
+
   def install
 	bin.install "formalfriday"
   end
