8ff425ea49a66e530a4669a9e5638bfa627b638a
Shay Bagants <shayb@jfrog.com>
2015-10-14 18:38:17 +0300

    Adding the formalfridayclub.rb

---
+++ formalfridayclub.rb
@@ -0,0 +1,11 @@
+class Formalfridayclub < Formula
+  desc ""
+  homepage "https://github.com/shaybagants/formalfridayclub"
+  url "https://github.com/shaybagants/formalfridayclub/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "3e43f927ce1d6e739130442769d77a0084d42d9e92f454d4375159ab7c41f4a1"
+
+  def install
+	bin.install "formalfriday"
+  end
+end
